5f6f8ba78cc9c1f4686e3e8c99c25743d2fe2b53
bmintz <bmintz@users.noreply.github.com>
2017-06-11 00:19:41 -0500

    spaces to tabs, change double to single quotes

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 import setuptools
 
 setuptools.setup(
-    name="poll_bot",
-    version="0.1.0",
-    url="https://github.com/bmintz/poll-bot",
+	name='poll_bot',
+	version='0.1.0',
+	url='https://github.com/bmintz/poll-bot',
 
-    author="Benjamin Mintz",
-    author_email="bmintz@protonmail.com",
+	author='Benjamin Mintz',
+	author_email='bmintz@protonmail.com',
 
-    description="A simple reaction-based Discord poll bot",
-    long_description=open('README.rst').read(),
+	description='A simple reaction-based Discord poll bot',
+	long_description=open('README.rst').read(),
 
-    packages=setuptools.find_packages(),
+	packages=setuptools.find_packages(),
 
-    install_requires=[],
+	install_requires=[],
 
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+	classifiers=[
+		'Development Status :: 2 - Pre-Alpha',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+	],
 )
