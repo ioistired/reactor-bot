9055f0ca03323b27183dcb8adc7dd9ac7b3f8f84
bmintz <bmintz@users.noreply.github.com>
2017-10-27 16:04:43 -0500

    don't send the same reaction twice
    to do this, keep a set of all the seen reactions, and don't send any in the set
    this is better than a list obviously, because `x in set` is O(1) avg case, and no
    worse than `x in list` worst case.
    
    Also encapsulate the try: react() except HTTPException: pass code so the command
    function is easier to read

--- reactor_bot/__init__.py
+++ reactor_bot/__init__.py
@@ -12,6 +12,8 @@ from reactor_bot import emoji
 import discord
 from discord.ext import commands
 
+import sys
+
 bot = commands.Bot(command_prefix='poll')
 
 
@@ -27,12 +29,18 @@ async def on_ready():
 @bot.command(name=':')
 async def reaction_poll(context):
 	message = context.message
-
+	seen_reactions = set()
 	for reaction in emoji.get_poll_emoji(message.content):
-		try:
-			await message.add_reaction(reaction)
-		except discord.errors.HTTPException:
-			# since we're trying to react with arbitrary emoji,
-			# some of them are going to be bunk
-			# but that shouldn't stop the whole poll
-			continue
+		if reaction not in seen_reactions:
+			seen_reactions.add(reaction)
+			await react_safe(message, reaction)
+
+
+async def react_safe(message, reaction):
+	try:
+		await message.add_reaction(reaction)
+	except discord.errors.HTTPException:
+		# since we're trying to react with arbitrary emoji,
+		# some of them are going to be bunk
+		# but that shouldn't stop the whole poll
+		pass
