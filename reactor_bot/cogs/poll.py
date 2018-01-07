c01ba67e9a474edc1b3c5056a036b5737d948312
bmintz <bmintz@users.noreply.github.com>
2018-01-06 19:08:29 -0600

    cogs: add stub Poll cog

---
+++ reactor_bot/cogs/poll.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from discord.ext import commands
+
+from reactor_bot import emoji_utils as emoji
+
+
+class Poll:
+	def __init__(self, bot):
+		self.bot = bot
+
+
