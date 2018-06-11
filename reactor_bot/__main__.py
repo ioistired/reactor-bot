1e3ca3c77f30a60f459a4f8ef46a1a7893cee918
bmintz <bmintz@protonmail.com>
2018-06-10 22:13:06 -0500

    __main__.py: fix the launcher script

--- reactor_bot/__main__.py
+++ reactor_bot/__main__.py
@@ -5,21 +5,26 @@ import logging
 
 from reactor_bot import bot
 
-# place the extensions in order of priority
-for extension in (
+def main():  # put it in a main function so that setuptools can create a launch script
+	# place the extensions in order of priority
+	for extension in (
+		'reactor_bot.cogs.db',
 		'reactor_bot.cogs.poll',
 		'reactor_bot.cogs.meta',
 		'jishaku',
 		'ben_cogs.admin',
 		'ben_cogs.stats',
-		'ben_cogs.misc'):
-	logging.info('Loading extension %s', extension)
-	try:
-		bot.load_extension(extension)
-	except Exception as e:
-		exc = '%s: %s' % (type(e).__name__, e)
-		logging.error('Failed to load extension %s', extension)
-		logging.error(exc)
-		continue
+		'ben_cogs.misc'
+	):
+		logging.info('Loading extension %s', extension)
+		try:
+			bot.load_extension(extension)
+		except Exception as e:
+			exc = '%s: %s' % (type(e).__name__, e)
+			logging.error('Failed to load extension %s', extension)
+			logging.error(exc)
+			continue
 
-bot.run(bot.config['tokens']['discord'])
+	bot.run(bot.config['tokens']['discord'])
+
+main()
